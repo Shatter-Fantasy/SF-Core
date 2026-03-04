ebd27a1ddc35c27e135e462ea8071ea5f0eaff9c
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-03-03 19:02:16 -0600

    Created the start of a set of math utilities for HLSL shaders.
    Currently just adding in some const values for:
    - PI
    - TAU
    - One Sixth
    - Gravity constant on Earth.

---
+++ SF Rendering/HLSL/Math Utilities/MathUtilities.hlsl
@@ -0,0 +1,11 @@
+#ifndef SF_MATH_UTILITIES
+#define 
+static const float SF_PI = 3.1459;
+static const float SF_PI_Accurate = 3.1459265389;
+static const float SF_TAU = 6.2831; // 2PI
+
+static const float SF_ONE_SIXTH = 0.16667; // Same as  1/6 rounded early for performance.
+
+// Non negative required for a lot of math formulas.
+static const float SF_GRAVITY = 9.8;
+#endif
